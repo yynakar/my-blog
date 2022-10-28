c5219a87863954a3a5dffe5de2616e0973f5c2bf
Ioanna Kartsonaki <yynakar.io@gmail.com>
2022-10-28 11:39:15 +0300

    Articles list page
    Added articles list in articles page with links

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,6 +1,16 @@
-const ArticlesListPage=()=>{
+import articles from './article-content';
+import { Link } from 'react-router-dom';
+const ArticlesListPage = () => {
     return (
-        <h1>This is the Articles Page!</h1>
+        <>
+            <h1>Articles</h1>
+            {articles.map(article =>
+                <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}</p>
+                </Link>
+            )}
+        </>
     );
 }
 
